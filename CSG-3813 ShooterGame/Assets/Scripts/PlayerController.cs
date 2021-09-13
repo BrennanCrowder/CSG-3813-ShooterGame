3c3bcfd0e779f6dfab4d5bc5c0265b0b51929c3d
Brennan Crowder <56270795+BrennanCrowder@users.noreply.github.com>
2021-09-13 16:41:14 -0500

    Movement Script and Player Sprite
    Added player sprite as well as movement script

---
+++ CSG-3813 ShooterGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+/*
+ * Created by: Brennan Crowder
+ * Date Created: 9/13/21
+ * 
+ * Last Edited by: Brennan Crowder
+ * Last Updated: 9/12/21
+ * 
+ * Description: Controls the player's movements 
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
